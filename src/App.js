04ebeb8113dc5335ed639392a57f73f9a27bee0e
PaoloRollo <paolo.rollo@laserromae.it>
2020-05-07 01:01:34 +0200

    added missing web3 handling.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useCallback } from 'react';
+import Disclaimer from './components/Disclaimer';
 import Header from './components/Header';
 import ERC20Faucet from './components/ERC20Faucet';
 import ERC721Faucet from './components/ERC721Faucet';
@@ -11,7 +12,12 @@ function App() {
 
   const web3Context = useWeb3Injected();
 
-  let { accounts, networkName, lib, connected } = web3Context;
+  let accounts, networkName, lib, connected = null;
+  let faucet721 = null;
+  if (web3Context) {
+    let { accounts, networkName, lib, connected } = web3Context;
+    let faucet721 = new lib.eth.Contract(erc721faucet, getERC721FaucetAddress(networkName));
+  }
 
   const requestAuth = async web3Context => {
     try {
@@ -26,14 +32,15 @@ function App() {
 
   requestAccess();
 
-  const faucet721 = new lib.eth.Contract(erc721faucet, getERC721FaucetAddress(networkName));
-
   return (
     <div className="App">
       <Header />
+      <div className="flex flex-col items-center justify-center mt-4 p-4">
+        <Disclaimer connected={connected} />
+      </div>
       <div className="flex flex-col md:flex-row items-center justify-center mt-4 p-4">
-        <ERC20Faucet currentNetwork={networkName} connected={connected} account={accounts[0]} web3={lib} />
-        <ERC721Faucet currentNetwork={networkName} connected={connected} account={accounts[0]} web3={lib} contract={faucet721} />
+        <ERC20Faucet currentNetwork={networkName} connected={connected} account={accounts ? accounts[0] : ''} web3={lib} />
+        <ERC721Faucet currentNetwork={networkName} connected={connected} account={accounts ? accounts[0] : ''} web3={lib} contract={faucet721} />
       </div>
       <Footer />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import logoSvg from '../assets/zinc.svg';
 export default class Header extends Component {
     render() {
         return(
-            <nav className="flex items-center justify-center flex-wrap bg-white p-2">
+            <nav className="flex items-center justify-center flex-wrap bg-white p-2 border-b border-gray-300 shadow mb-4">
                 <div className="flex items-center flex-shrink-0 text-white">
                     <img src={logoSvg} width={96} height={96} alt="" />
                 </div>
